e3464bfb6168750cd7a70ca402e219fd23ba2d37
JBrozinski <joseph.brozinski@gmail.com>
2020-03-08 14:37:17 -0600

    basic functions for buttons. showing results in console log

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,46 @@
-$("button").on("click", function() {
-  var x = $(this).data("search");
-  console.log(x);
+var btnList = ["dog", "cat", "snake"];
+
+displayButtons();
+
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+  var input = $("#input").val();
+  console.log(input);
 });
+
+function handleClick() {
+  var x = $(this).data("search");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    x +
+    "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+  $.ajax({
+    method: "GET",
+    url: queryURL
+  }).then(function(data) {
+    handleResponse(data);
+  });
+}
+
+// var response = data (because data is passed in function call)
+function handleResponse(response) {
+  console.log(response);
+}
+function displayButtons() {
+  // document.getElementById("buttons").innerHTML = "";
+  $("#buttons").empty();
+  for (var i = 0; i < btnList.length; i++) {
+    // var btn = document.createElement("button");
+    // btn.textContent = btnList[i];
+    // btn.setAttribute("data-search", btnList[i]);
+
+    // document.getElementById("buttons").appendChild(btn);
+
+    var btn = $("<button>")
+      .text(btnList[i])
+      .attr("data-search", btnList[i])
+      .on("click", handleClick);
+
+    $("#buttons").append(btn);
+  }
+}
--- javascript/app.js
+++
@@ -1,8 +0,0 @@
-$("button").on("click", function() {
-  var x = $(this).data("search");
-  //   console.log(x);
-
-  var queryURL = "bob";
-  "https://giphy.com/v1/gifs/search?q=" + x + "api_key=...&limit=10";
-  console.log(queryURL);
-});
